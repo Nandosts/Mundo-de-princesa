f2c60ad27c6d06cf88c1e2246bce1d5dd7ee2f57
Nandosts <fernando.jsts@gmail.com>
2023-04-09 20:12:02 -0300

    Carrega produtos com base no rolamento da página

---
+++ app/javascript/components/Loading.svelte
@@ -0,0 +1,39 @@
+<div class="loading">
+  <div class="loading-spinner" />
+</div>
+
+<style>
+  /* Estilize o container do loading */
+  .loading {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(255, 255, 255, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+  }
+
+  /* Estilize o spinner do loading */
+  .loading-spinner {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 3px solid #007bff; /* Cor do spinner */
+    border-top-color: transparent;
+    animation: loading-spinner 0.8s linear infinite;
+  }
+
+  /* Animação do spinner */
+  @keyframes loading-spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
--- app/javascript/components/pages/Hello.svelte
+++ app/javascript/components/pages/Hello.svelte
@@ -1,40 +1,81 @@
 <script lang="ts">
   import CartaoProduto from "../CartaoProduto.svelte";
+  import Loading from "../Loading.svelte";
 
-  type TLista = {
+  type TProduto = {
     id: string;
     nome: string;
     descricao: string;
     preco: string;
     quantidade: number;
     imageUrl: string;
-  }[];
+  };
+  export let lista: TProduto[] = [];
+  let produtos: TProduto[] = [];
+  let larguraPorProduto = 250; // largura de cada Produto de cartões em pixels
+  let gapEntreProdutos = 16; // espaçamento (gap) entre os produtos em pixels
+  let numeroDeColunas = 3; // Número de colunas desejadas
+  let produtosPorPagina = Math.ceil(
+    (window.innerWidth / (larguraPorProduto + gapEntreProdutos)) *
+      numeroDeColunas
+  );
 
-  export let lista: TLista = [
-    {
-      id: "1",
-      nome: "Produto 1",
-      descricao: "Descrição 1",
-      preco: '1000',
-      quantidade: 1,
-      imageUrl: "https://via.placeholder.com/150",
-    },
-  ];
+  let carregarMais = false;
+  let mostrandoLoading = false; // Variável para controlar se o loading está sendo mostrado
+
+  function carregarMaisProdutos() {
+    mostrandoLoading = true; // Ativa o indicador de loading
+    setTimeout(() => {
+      const startIndex = produtos.length;
+      const endIndex = startIndex + produtosPorPagina;
+      if (endIndex <= lista.length) {
+        produtos = [...produtos, ...lista.slice(startIndex, endIndex)];
+      } else if (startIndex < lista.length && endIndex > lista.length) {
+        produtos = [...produtos, ...lista.slice(startIndex)];
+      }
+      carregarMais = false;
+      mostrandoLoading = false; // Desativa o indicador de loading
+    }, 1000); // Atraso de 1 segundo (1000 milissegundos)
+  }
+
+  function verificarRolagem() {
+    const alturaJanela = window.innerHeight;
+    const alturaPagina = document.documentElement.scrollHeight;
+    const scrollTop =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    const margemErro = 0.9;
+    const alturaParaCarregar = alturaPagina * margemErro;
+
+    if (alturaJanela + scrollTop >= alturaParaCarregar && !carregarMais) {
+      carregarMais = true;
+      carregarMaisProdutos();
+    }
+  }
+
+  window.addEventListener("scroll", verificarRolagem);
+
+  produtos = lista.slice(0, produtosPorPagina);
 </script>
 
 <h1>Bem-vindo ao Mundo de Princesa!</h1>
 <p>Aqui estão alguns produtos incríveis para você escolher:</p>
 
 <div class="grid-container">
-  {#each lista as produto}
+  {#each produtos as produto}
     <div class="grid-item">
       <CartaoProduto {produto} />
     </div>
   {/each}
 </div>
 
+{#if mostrandoLoading}
+  <Loading />
+{/if}
+
 <style lang="scss">
-  /* Estilize a lista de itens */
   .grid-container {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
@@ -47,13 +88,11 @@
     justify-content: center;
   }
 
-  /* Estilize o cabeçalho */
   h1 {
     margin-bottom: 16px;
     color: var(--texto-principal);
   }
 
-  /* Estilize o parágrafo */
   p {
     margin-bottom: 16px;
   }
