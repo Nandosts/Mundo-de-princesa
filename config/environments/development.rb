82c7046401c05f2024847dc2156a036e0ea547b8
Nandosts <fernando.jsts@gmail.com>
2023-04-16 14:29:33 -0300

    Adiciona host do railway

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  # railsway host
+  config.hosts += ['mundo-de-princesa-production.up.railway.app']
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  # railsway host
+  config.hosts += ['mundo-de-princesa-production.up.railway.app']
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
