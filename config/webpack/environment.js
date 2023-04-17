ab878dd9fd75cade40ca6c79a13cd1fad331b505
Nandosts <fernando.jsts@gmail.com>
2023-04-17 06:47:26 -0300

    Adiciona resolve ao webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require("@rails/webpacker");
 const svelte = require("./loaders/svelte");
 
+environment.config.resolve = {
+  conditionNames: ['webpack', 'browser', 'web', 'svelte'],
+  // outras configurações
+};
 environment.loaders.prepend("svelte", svelte);
 module.exports = environment;
