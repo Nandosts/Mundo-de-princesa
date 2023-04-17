652a7fa8a4b6b4cae5a5ef850c8693e04b132d09
Nandosts <fernando.jsts@gmail.com>
2023-04-17 06:53:54 -0300

    então tá bom

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,5 @@
 const { environment } = require("@rails/webpacker");
 const svelte = require("./loaders/svelte");
 
-environment.config.resolve = {
-  conditionNames: ['webpack', 'browser', 'web', 'svelte'],
-  // outras configurações
-};
 environment.loaders.prepend("svelte", svelte);
 module.exports = environment;
